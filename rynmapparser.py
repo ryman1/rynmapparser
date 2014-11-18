8cdb9d48bdca4de9508f9981b11b1a09b8bdbdc8
bandrel <justin@jbollinger.com>
2014-11-17 21:08:26 -0500

    changed output to strip out the downed hosts

--- rynmapparser.py
+++ rynmapparser.py
@@ -3,13 +3,28 @@ import xml.dom.minidom
 import sys
 import getopt
 import time
-try: 
-    scandata = sys.argv[1]
-except:
-    print "*** You need to supply an Nmap XML file ***"
-if scandata:
-    doc = xml.dom.minidom.parse(scandata)
-    output = []
+import csv
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"hi:o:",["inputfile=","outputfile="])
+except getopt.GetoptError:
+    print 'rynnmapparser.py -i <inputfile> -o [outputfile] '
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == '-h':
+        print 'rynnmapparser.py -i <inputfile> -o [outputfile] '
+        sys.exit()
+    elif opt in ("-i", "--inputfile"):
+        inputfile = arg
+    elif opt in ("-o", "--outputfile"):
+        outputfile = arg
+
+doc = xml.dom.minidom.parse(inputfile)
+output = []
+
+with open(outputfile, 'w') as csvfile:
+    csvout = csv.writer(csvfile, delimiter=",",quotechar="'", quoting=csv.QUOTE_MINIMAL)
+    csvout.writerow(['State','IP','type','vendor','OS Family','OS Generation','OS Class Accuracy','Name', 'CPE',
+                    'Name Accuracy'])
     for host in doc.getElementsByTagName("host"):
         ip = ''
         type = ''
@@ -27,7 +42,7 @@ if scandata:
         notBefore = ''
         notAfter = ''
         addresses = host.getElementsByTagName("address")
-        ip = addresses[0].getAttribute("addr")                         # Get IP address from addr element 
+        ip = addresses[0].getAttribute("addr")                         # Get IP address from addr element
         oss = host.getElementsByTagName("os")
         for os in oss:
             for osclass in os.getElementsByTagName("osclass"):
@@ -46,17 +61,15 @@ if scandata:
                         cpe = osclass.getElementsByTagName("cpe")[0].childNodes[0].nodeValue
                     except:
                         pass
-                    
-                        
+
             for osmatches in os.getElementsByTagName("osmatch"):
-                name = osmatches.getAttribute("name")
-                nameaccuracy = osmatches.getAttribute("accuracy")
-                
-        for st in host.getElementsByTagName("status"):
-            state = st.getAttribute("state")
+                 name = osmatches.getAttribute("name")
+                 nameaccuracy = osmatches.getAttribute("accuracy")
 
-
-        
-        output.append(state + '\t' + ip + '\t' + type + '\t' + vendor + '\t' + osfamily + '\t' + osgen + '\t' + osclassaccuracy + '\t' + name + '\t' + cpe + '\t' + nameaccuracy)
-    for i in output:
-        print i
+            for st in host.getElementsByTagName("status"):
+                state = st.getAttribute("state")
+        output.append([str(state),str(ip),str(type),str(vendor),str(osfamily),str(osgen),str(osclassaccuracy),str(name),str(cpe),str(nameaccuracy)])
+    for row in output:
+        if row[0] is not '':
+            csvout.writerow(row)
+            print row

f068f2db5ce6d8fcdc857e8f6bd8b6c65d3a74f2
ryman1 <ryman1@gmail.com>
2014-11-25 00:40:13 -0500

    beginning work on adding more scanning features
    added the ability to gather multiple addresses instead of just a single
    ip address.
    
    Also cleaned up the variable declarations a bit.

--- rynmapparser.py
+++ rynmapparser.py
@@ -22,21 +22,29 @@ output = []
 
 with open(outputfile, 'w') as csvfile:
     csvout = csv.writer(csvfile, delimiter=",",quotechar="'", quoting=csv.QUOTE_MINIMAL)
-    csvout.writerow(['State','IP','type','vendor','OS Family','OS Generation','OS Class Accuracy','Name', 'CPE',
-                    'Name Accuracy'])
+    csvout.writerow(['State', 'IP', 'type', 'vendor', 'OS Family', 'OS Generation', 'OS Class Accuracy', 'Name', 'CPE', 'Name Accuracy'])
+    
+    #go through every host element in the xml document
     for host in doc.getElementsByTagName("host"):
-        ip = ''
-        type = ''
-        vendor = ''
-        osfamily = ''
-        osgen = ''
-        osclassaccuracy = ''
-        name = ''
-        nameaccuracy = ''
-        cpe = ''
-        addresses = host.getElementsByTagName("address")
-        ip = addresses[0].getAttribute("addr")                         # Get IP address from addr element
+        ip = type = vendor = osfamily = osgen = osclassaccuracy = name = nameaccuracy = cpe = ''
+        
+        
+        #Gather addresses
+        #addressdict can store multiple types of addresses i.e. ipv4, ipv6, mac, etc.
+        addressesdict = {}
+        
+        for address in host.getElementsByTagName("address"):
+            addrtype = address.getAttribute("addrtype")
+            
+            #if there are multiple addresses of the same type, keep them in the same cell and separate with a newline. For instance, if there are multiple ip addresses, this will be used.
+            if addressesdict.has_key(addrtype):
+                addressesdict.update(addrtype : addressesdict[addrtype] + "\n" + address.getAttribute("addr")})
+            else:
+                addressesdict.update({address.getAttribute("addrtype"):address.getAttribute("addr")})
+                
         oss = host.getElementsByTagName("os")
+        for st in host.getElementsByTagName("status"):
+            state = st.getAttribute("state")
         for os in oss:
             for osclass in os.getElementsByTagName("osclass"):
                 if type == '':
@@ -57,8 +65,6 @@ with open(outputfile, 'w') as csvfile:
             for osmatches in os.getElementsByTagName("osmatch"):
                  name = osmatches.getAttribute("name")
                  nameaccuracy = osmatches.getAttribute("accuracy")
-            for st in host.getElementsByTagName("status"):
-                state = st.getAttribute("state")
 
         output.append([str(state),str(ip),str(type),str(vendor),str(osfamily),str(osgen),str(osclassaccuracy),str(name),str(cpe),str(nameaccuracy)])
     for row in output:
